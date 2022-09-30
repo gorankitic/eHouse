74bcb6a85cd1935d3d53053739612ac42ebe2165
Goran Kitic <kiticgoran90@gmail.com>
2022-09-30 20:12:51 +0200

    Error handling update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,22 +1,23 @@
 const Product = require('../models/productModel.js')
+const AppError = require('../utils/appError.js')
+const catchAsync = require('../utils/catchAsync')
 
-const getProducts = async (req, res, next) => {
+const getProducts = catchAsync(async (req, res, next) => {
     const products = await Product.find()
     res.status(200).json(products)
-}
+})
 
-const getProduct = async (req, res, next) => {
+const getProduct = catchAsync(async (req, res, next) => {
     const product = await Product.findById(req.params.id)
-    res.status(200).json(product)
-}
-
-const createProduct = async (req, res, next) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json(product)
-    } catch(error) {
-        console.log(error)
+    if(!product) {
+        return next(new AppError('No product found with that ID.', 404))
     }
-}
+    res.status(200).json(product)
+})
+
+const createProduct = catchAsync(async (req, res, next) => {
+    const product = await Product.create(req.body)
+    res.status(201).json(product)
+})
 
 module.exports = { getProducts, getProduct, createProduct }
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
 const express = require('express')
 require('dotenv').config({path: '../.env'})
 const chalk = require('chalk')
-const connectDatabase = require('./database.js')
-const productRouter = require('./routes/productRouter.js')
+const connectDatabase = require('./database')
+const productRouter = require('./routes/productRouter')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 
 const app = express()
 app.use(express.json());
 
 connectDatabase()
 
+// Routes
 app.use('/api/products', productRouter)
+
+// Handling unhandled routes
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404))
+})
+
+// Global error handling middleware
+app.use(globalErrorHandler)
 
 app.listen(process.env.PORT, () => {
     console.log(chalk.bgGreen(`Server is up in ${process.env.NODE_ENV} mode on port ${process.env.PORT}.`))
